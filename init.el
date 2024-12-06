3c73e3ada22b8536849e0346bf568b6e30c8d5d6
masmarius <masmarius@users.noreply.github.com>
2024-12-06 14:48:38 -0600

    Update init.el

--- init.el
+++ init.el
@@ -4,8 +4,8 @@
 
 ;; initial frame and default window
 
-(add-to-list 'default-frame-alist '(height . 45)) ;initial frame size
-(add-to-list 'default-frame-alist '(width . 120))
+;(add-to-list 'default-frame-alist '(height . 45)) ;initial frame size
+;(add-to-list 'default-frame-alist '(width . 120))
 (push '(background-color . "#fffff8") default-frame-alist) ;light bg color
 
 (tooltip-mode -1)
